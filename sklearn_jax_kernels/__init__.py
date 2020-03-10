f9f631e10f98c2ecc116aa81672e66d5346a3d5e
Max Horn <max.horn@bsse.ethz.ch>
2020-03-10 20:19:24 +0100

    Ignore egg folder and fix missing Kernel in __init__

--- sklearn_jax_kernels/__init__.py
+++ sklearn_jax_kernels/__init__.py
@@ -1,7 +1,9 @@
 """Jax implementation of sklearn kernels."""
-from base_kernels import (
-    ConstantKernel, Exponentiation, Kernel, Product, RBF, Sum)
+from .base_kernels import (
+    ConstantKernel, Exponentiation, Kernel, KernelOperator, Product, RBF, Sum)
 
 __all__ = [
-    'ConstantKernel', 'Exponentiation', 'Kernel', 'Product', 'RBF', 'Sum']
+    'ConstantKernel', 'Exponentiation', 'Kernel', 'KernelOperator', 'Product',
+    'RBF', 'Sum'
+]
 __version__ = '0.1.0'
