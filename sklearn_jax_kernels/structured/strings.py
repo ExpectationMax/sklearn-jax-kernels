7f4c6472dc669d1d0cea420733e93a03b4fa5a16
Max Horn <max.horn@bsse.ethz.ch>
2020-03-12 11:07:02 +0100

    Add DistanceSpectrumKernel

--- sklearn_jax_kernels/structured/strings.py
+++ sklearn_jax_kernels/structured/strings.py
@@ -1,10 +1,12 @@
 """Implementation of string kernels."""
+from functools import partial
+from jax import vmap
 import jax.numpy as np
 from jax.lax import dynamic_slice_in_dim
 from jax.experimental import loops
 
 from sklearn_jax_kernels import Kernel
-from sklearn.gaussian_process.kernels import GenericKernelMixin
+from sklearn.gaussian_process.kernels import GenericKernelMixin, Hyperparameter
 
 
 class SpectrumKernel(GenericKernelMixin, Kernel):
@@ -114,6 +116,151 @@ class SpectrumKernel(GenericKernelMixin, Kernel):
 
         """
         return np.empty((0, 2))
+
+    def is_stationary(self):
+        """Whether this kernel is stationary."""
+        return False
+
+
+class DistanceSpectrumKernel(Kernel):
+    """Spectrum kernel weighting ngrams by their distance in the sequence."""
+
+    def __init__(self, distance_kernel: Kernel, n_gram_length):
+        """Initialize DistanceSpectrumKernel using distance_kernel.
+
+        Args:
+            distance_kernel (Kernel): Kernel used to quantify distance of kmers
+                in the string.
+
+        """
+        self.distance_kernel = distance_kernel
+        self.n_gram_length = n_gram_length
+
+    @property
+    def pure_kernel_fn(self):
+        """Return the pure fuction for computing the kernel."""
+        n_gram_length = self.n_gram_length
+
+        distance_kernel = self.distance_kernel.pure_kernel_fn
+
+        if n_gram_length is None:
+            # Assume input is kmer transformed
+            def kernel_fn(theta, kmers1, kmers2):
+                pos_kernel = partial(distance_kernel, theta)
+                kmer2_offsets = np.arange(kmers1.shape[0], dtype=np.uint32)
+                with loops.Scope() as s:
+                    s.out = 0.
+                    for i in s.range(kmers1.shape[0]):
+                        kmer = kmers1[i]
+                        distances = vmap(
+                            lambda j: pos_kernel(i, j))(kmer2_offsets)
+                        is_same = np.all(kmer[None, :] == kmers2, axis=1)
+                        n_matches = np.sum(is_same * distances)
+                        s.out += n_matches
+                return s.out
+        else:
+            def kernel_fn(theta, string1, string2):
+                pos_kernel = partial(distance_kernel, theta)
+                with loops.Scope() as s1:
+                    s1.out = 0.
+                    for i in s1.range(len(string1) - n_gram_length + 1):
+                        with loops.Scope() as s2:
+                            s2.out = 0.
+                            for j in s2.range(len(string2) - n_gram_length + 1):
+                                substring1 = dynamic_slice_in_dim(
+                                    string1, i, n_gram_length)
+                                substring2 = dynamic_slice_in_dim(
+                                    string2, j, n_gram_length)
+                                s2.out += (
+                                    np.all(substring1 == substring2) *
+                                    pos_kernel(i, j)
+                                )
+                        s1.out += s2.out
+                return s1.out
+
+        return kernel_fn
+
+    @property
+    def hyperparameters(self):
+        """Return a list of all hyperparameter."""
+        r = []
+        for hyperparameter in self.kernel.hyperparameters:
+            r.append(Hyperparameter("distance_kernel__" + hyperparameter.name,
+                                    hyperparameter.value_type,
+                                    hyperparameter.bounds,
+                                    hyperparameter.n_elements))
+        return r
+
+    @property
+    def theta(self):
+        """Return the (flattened, log-transformed) non-fixed hyperparameters.
+
+        Note that theta are typically the log-transformed values of the
+        kernel's hyperparameters as this representation of the search space
+        is more amenable for hyperparameter search, as hyperparameters like
+        length-scales naturally live on a log-scale.
+
+        Returns:
+            theta : array, shape (n_dims,)
+                The non-fixed, log-transformed hyperparameters of the kernel
+
+        """
+        return self.distance_kernel.theta
+
+    @theta.setter
+    def theta(self, theta):
+        """Set the (flattened, log-transformed) non-fixed hyperparameters.
+
+        Parameters:
+            theta : array, shape (n_dims,)
+                The non-fixed, log-transformed hyperparameters of the kernel
+
+        """
+        self.distance_kernel.theta = theta
+
+    @property
+    def bounds(self):
+        """Return the log-transformed bounds on the theta.
+
+        Returns:
+            bounds : array, shape (n_dims, 2)
+                The log-transformed bounds on the kernel's hyperparameters
+                theta
+
+        """
+        return self.distance_kernel.bounds
+
+    def get_params(self, deep=True):
+        """Get parameters of this kernel.
+
+        Parameters:
+            deep : boolean, optional
+                If True, will return the parameters for this estimator and
+                contained subobjects that are estimators.
+
+        Returns:
+            params : mapping of string to any
+                Parameter names mapped to their values.
+
+        """
+        params = dict(
+            distance_kernel=self.distance_kernel,
+            n_gram_length=self.n_gram_length
+        )
+        if deep:
+            deep_items = self.kernel.get_params().items()
+            params.update(
+                ('distance_kernel__' + k, val) for k, val in deep_items)
+        return params
+
+    def __eq__(self, b):
+        """Whether two instances are considered equal."""
+        if type(self) != type(b):
+            return False
+        return (
+            self.distance_kernel == b.distance_kernel and
+            self.n_gram_length == b.n_gram_length
+        )
 
     def is_stationary(self):
         """Whether this kernel is stationary."""
--- tests/test_structured_strings.py
+++ tests/test_structured_strings.py
@@ -1,7 +1,10 @@
 """Test string kernels and utilities associated with them."""
 import numpy as np
-from sklearn_jax_kernels.structured.string_utils import AsciiBytesTransformer
-from sklearn_jax_kernels.structured.strings import SpectrumKernel
+from sklearn_jax_kernels.structured.string_utils import (
+    AsciiBytesTransformer, NGramTransformer)
+from sklearn_jax_kernels import RBF
+from sklearn_jax_kernels.structured.strings import (
+    DistanceSpectrumKernel, SpectrumKernel)
 
 
 class TestUtils:
@@ -14,9 +17,46 @@ class TestUtils:
 
 
 class TestKernels:
-    def test_spectrum_kernel(self):
+    def test_spectrum_kernel_example(self):
         strings = ['aabbcc', 'aaabac']
         strings_transformed = AsciiBytesTransformer().transform(strings)
         kernel = SpectrumKernel(n_gram_length=2)
         K = kernel(strings_transformed)
         assert np.allclose(K, np.array([[5., 3.], [3., 7.]]))
+
+    def test_spectrum_kernel_ngram_transform(self):
+        n_gram_length = 2
+        strings = ['aabbcc', 'aaabac']
+        strings_transformed = AsciiBytesTransformer().transform(strings)
+        ngrams = NGramTransformer(n_gram_length).transform(strings_transformed)
+
+        kernel_strings = SpectrumKernel(n_gram_length=n_gram_length)
+        kernel_ngrams = SpectrumKernel(n_gram_length=None)
+        K_strings = kernel_strings(strings_transformed)
+        K_ngrams = kernel_ngrams(ngrams)
+        assert np.allclose(K_strings, K_ngrams)
+
+    def test_distance_spectrum_kernel_ngram_transform(self):
+        n_gram_length = 2
+        distance_kernel = RBF(1.0)
+        strings = ['aabbcc', 'aaabac']
+        strings_transformed = AsciiBytesTransformer().transform(strings)
+        ngrams = NGramTransformer(n_gram_length).transform(strings_transformed)
+
+        kernel_strings = DistanceSpectrumKernel(distance_kernel, n_gram_length)
+        kernel_ngrams = DistanceSpectrumKernel(distance_kernel, None)
+        K_strings = kernel_strings(strings_transformed)
+        K_ngrams = kernel_ngrams(ngrams)
+        assert np.allclose(K_strings, K_ngrams)
+
+    def test_distance_spectrum_kernel(self):
+        distance_kernel = RBF(1.0)
+        strings = ['aabbcc', 'aaabac']
+        strings_transformed = AsciiBytesTransformer().transform(strings)
+        kernel = DistanceSpectrumKernel(distance_kernel, 2)
+        K = kernel(strings_transformed)
+        K_gt = np.array([
+             [5.,        2.2130613],
+             [2.2130613, 6.2130613]
+        ])
+        assert np.allclose(K, K_gt)
