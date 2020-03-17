874f0952bc39d27e896d1d3217c7f996c849fe58
Max Horn <max.horn@bsse.ethz.ch>
2020-03-17 09:14:49 +0100

    Move utils to pure numpy

--- sklearn_jax_kernels/structured/string_utils.py
+++ sklearn_jax_kernels/structured/string_utils.py
@@ -1,8 +1,5 @@
 """Utilities for usage with strings."""
-from functools import partial
 import numpy as np
-import jax.numpy as jnp
-from jax import vmap
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -18,10 +15,10 @@ class AsciiBytesTransformer(TransformerMixin):
     def transform(self, X):
         if len(np.unique([len(x) for x in X])) == 1:
             # All strings are of same length
-            return jnp.asarray([bytearray(x, 'ascii', 'strict') for x in X])
+            return np.asarray([bytearray(x, 'ascii', 'strict') for x in X])
 
         # Varying length
-        return [jnp.asarray(bytearray(a, 'ascii', 'strict')) for a in X]
+        return [np.asarray(bytearray(a, 'ascii', 'strict')) for a in X]
 
     def inverse_transform(self, X):
         return [str(bytes(a), 'ascii', 'strict') for a in X]
@@ -31,14 +28,13 @@ class NGramTransformer(TransformerMixin, BaseEstimator):
     def __init__(self, ngram_length):
         self.ngram_length = ngram_length
 
-    @staticmethod
-    def build_kmers(nmers, string):
-        n = string.shape[0]
-        return jnp.stack(
-            [string[i:1+n+i-nmers] for i in range(0, nmers)], axis=1)
-
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        return vmap(partial(self.build_kmers, self.ngram_length))(X)
+        n = X.shape[1]
+        ngram_slices = [
+            X[:, i:n+1-self.ngram_length+i]
+            for i in range(0, self.ngram_length)
+        ]
+        return np.stack(ngram_slices, axis=1)
